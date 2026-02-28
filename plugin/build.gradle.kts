cdf58776dc915b10b1ee28a883d9fd8ce543802c
rakesh-123-source <radheshyaam.gazol.1111@gmail.com>
2026-02-28 12:22:18 +0530

    fix: restore version in plugin build, let lavalink gradle plugin handle JAR packaging

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -1,14 +1,14 @@
 plugins {
     java
-    `maven-publish`
     alias(libs.plugins.lavalink)
 }
+
+version = "3.0.0"
 
 lavalinkPlugin {
     name = "slugyzeon-plugin"
     apiVersion = libs.versions.lavalink.api
     serverVersion = libs.versions.lavalink.server
-    configurePublishing = false
 }
 
 java {
@@ -25,13 +25,4 @@ tasks {
 dependencies {
     compileOnly("com.fasterxml.jackson.core:jackson-databind:2.17.0")
     implementation(project(":slugyzeon-main"))
-}
-
-publishing {
-    publications {
-        create<MavenPublication>("maven") {
-            from(components["java"])
-            artifactId = "slugyzeon-plugin"
-        }
-    }
 }
